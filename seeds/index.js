8ab0b034214213f0f0127e6e70738c38cba29942
Debanjan Poddar <debanjan2602@gmail.com>
2022-09-21 01:01:24 +0530

    Added placeholder image & description in seed file

--- seeds/index.js
+++ seeds/index.js
@@ -16,10 +16,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random = Math.floor(Math.random() * 1000);
-
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random].city}, ${cities[random].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni temporibus commodi eum in consequatur tenetur mollitia iste culpa, molestiae quidem minus illum ipsa porro, dicta doloribus sit aliquid officiis repudiandae.",
+            price: price
         });
         await camp.save();
     }
