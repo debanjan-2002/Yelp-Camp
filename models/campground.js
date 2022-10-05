6348faa4d33a3397442806f8770df5e7cec095a0
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-05 16:16:50 +0530

    Added geometry property in campground schema

--- models/campground.js
+++ models/campground.js
@@ -11,24 +11,44 @@ ImageSchema.virtual("thumbnail").get(function () {
     return this.url.replace("/upload", "/upload/w_200");
 });
 
-const CampgroundSchema = new Schema({
-    title: String,
-    images: [ImageSchema],
-    price: Number,
-    description: String,
-    location: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Review"
-        }
-    ]
-});
+const ops = { toJSON: { virtuals: true } };
 
+const CampgroundSchema = new Schema(
+    {
+        title: String,
+        images: [ImageSchema],
+        // geometry is different because it is a pattern for geoJSON
+        geometry: {
+            type: {
+                type: String,
+                enum: ["Point"],
+                required: true
+            },
+            coordinates: {
+                type: [Number],
+                required: true
+            }
+        },
+        price: Number,
+        description: String,
+        location: String,
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Review"
+            }
+        ]
+    },
+    ops
+);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+    return `<a href="/campgrounds/${this._id}">${this.title}</a>`;
+});
 CampgroundSchema.post("findOneAndDelete", async doc => {
     if (doc) {
         await Review.deleteMany({
